b0f9759a5c7e22bc3af6f92915a962695a2da1f6
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2018-06-04 09:57:59 +0300

    unit test for store progressed

--- ConsignmentShop/Tests/StoreTest.cs
+++ ConsignmentShop/Tests/StoreTest.cs
@@ -62,22 +62,37 @@ namespace Tests
                 }.ToList()
             };
 
-            // передаем в конструктор тип класса
-            XmlSerializer formatter = new XmlSerializer(typeof(Store));
+            //// передаем в конструктор тип класса
+            //var formatter = new XmlSerializer(typeof(Store));
 
-            // получаем поток, куда будем записывать сериализованный объект
-            using (FileStream fs = new FileStream("perfect_store.xml", FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, store);
-            }
+            //// получаем поток, куда будем записывать сериализованный объект
+            //using (var fs = new FileStream("perfect_store.xml", FileMode.OpenOrCreate))
+            //{
+            //    formatter.Serialize(fs, store);
+            //}
 
-            return;
+            //return;
 
             // action
             var str = NaiveXMLSerializer.Serialize(store);
             File.WriteAllText(fileName, str);
 
             // assert
+            var serializer = new XmlSerializer(typeof(Store));
+            var reader = new StreamReader(fileName);
+            var targetStore = (Store)serializer.Deserialize(reader);
+            reader.Close();
+
+            Assert.IsNotNull(targetStore);
+            Assert.AreEqual(targetStore.Name, store.Name);
+
+            Assert.AreEqual(targetStore.Vendors.Count(), 1);
+            Assert.AreEqual(targetStore.Vendors.First().Display, store.Vendors.First().Display);
+            Assert.AreEqual(targetStore.Vendors.First().FirstName, store.Vendors.First().FirstName);
+            Assert.AreEqual(targetStore.Vendors.First().LastName, store.Vendors.First().LastName);
+            Assert.AreEqual(targetStore.Vendors.First().PaymentDue, store.Vendors.First().PaymentDue);
+
+            Assert.AreEqual(targetStore.Items.Count(), 2);
         }
     }
 }
